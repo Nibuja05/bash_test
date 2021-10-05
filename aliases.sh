5eea39b041572c87f7279c1d4da9650b8d7a663f
Anton Winkler <antonwinkler98@web.de>
2021-10-05 19:42:43 +0200

    added ghelp command

--- aliases.sh
+++ aliases.sh
@@ -75,6 +75,39 @@ prfixes () {
 	ga && gc "$C_NAME" && gp
 }
 
+pushchanges () {
+	C_NAME="update"
+	if [ "$1" != "" ]; then
+		C_NAME=$1
+	fi
+	ga && gc "$C_NAME" && gp
+}
+
+ghelp () {
+	echo -e "\033[96mCustom Git Commands Help \033[0m"
+	echo -e "\n\033[42;30m g \033[0m as alias for all \033[4mgit\033[0m commands\n"
+	echo -e "\033[42;30m gs \033[0m       \033[4mgit status\033[0m"
+	echo -e "\033[42;30m gc \033[0m       \033[4mgit commit -m\033[0m [COMMIT_MSG]"
+	echo -e "\033[42;30m gp \033[0m       \033[4mgit push\033[0m"
+	echo -e "\033[42;30m gpup \033[0m     \033[4mgit push --set-upstream origin\033[0m"
+	echo -e "\033[42;30m gu \033[0m       \033[4mgit pull\033[0m"
+	echo -e "\033[42;30m gm \033[0m       \033[4mgit merge\033[0m"
+	echo -e "\033[42;30m cont \033[0m     \033[4mgit merge --continue\033[0m"
+	echo -e "\033[42;30m update \033[0m   \033[4mgit fetch origin\033[0m"
+	echo ""
+	echo -e "\033[42;30m switch \033[0m + [BRANCH_NAME]            switch to a different branch"
+	echo -e "\033[42;30m master \033[0m                            switch to the main branch"
+	echo -e "\033[42;30m newbranch \033[0m + [BRANCH_NAME]         create a new branch and push it"
+	echo -e "\033[42;30m newmasterbranch \033[0m + [BRANCH_NAME]   create a new branch (from master)"
+	echo "                                    and push it"
+	echo -e "\033[42;30m mergemaster \033[0m                       update branch from master"
+	echo -e "\033[42;30m curbranch \033[0m                         print current branch name"
+	echo -e "\033[42;30m prfixes \033[0m + [COMMIG_MSG]            add, commit and push changes"
+	echo "                                    defaults to 'PR fixes'"
+	echo -e "\033[42;30m pushchanges \033[0m + [COMMIG_MSG]        add, commit and push changes"
+	echo "                                    defaults to 'update'"
+}
+
 case "$TERM" in
 xterm*)
 	# The following programs are known to require a Win32 Console
